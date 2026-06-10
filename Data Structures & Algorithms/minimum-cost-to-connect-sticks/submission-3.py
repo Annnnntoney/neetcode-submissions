85ce9ff6c88cee2384f6daac3a2171776c4e0500
hachi <145106207+Annnnntoney@users.noreply.github.com>
2026-06-10 23:10:31 +0800

    Add: minimum-cost-to-connect-sticks - submission-3

---
+++ Data Structures & Algorithms/minimum-cost-to-connect-sticks/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def connectSticks(self, sticks: List[int]) -> int:
+        heapq.heapify(sticks)
+        cost = 0
+
+        while len(sticks) > 1:
+            a = heapq.heappop(sticks)
+            b = heapq.heappop(sticks)
+
+            cost += a+b
+            heapq.heappush(sticks, a+b)
+        return cost
